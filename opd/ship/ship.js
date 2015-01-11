7ee3a83b52338a4529a1fea3bf7dcc659ce712a3
Mr21 <thomastortorini@gmail.com>
2015-01-11 05:05:52 +0100

    ship gravity calcul

--- opd/ship/ship.js
+++ opd/ship/ship.js
@@ -2,7 +2,7 @@ opd.ship = {
 	init: function(cnv, ctx) {
 		this.x = 50;
 		this.y = -150;
-		this.dx = 50;
+		this.dx = 0;
 		this.dy = 0;
 		this.spShip = new canvaslothSprite(ctx, cnv.image('spShip.png'))
 			.pivotX("center").pivotY("center")
@@ -55,15 +55,28 @@ opd.ship = {
 				-this.y
 			),
 			vx = Math.sin(angle),
-			vy = Math.cos(angle);
+			vy = Math.cos(angle),
+			vx90 = Math.sin(angle - Math.PI / 2),
+			vy90 = Math.cos(angle + Math.PI / 2),
+			dist = this.x * this.x + this.y * this.y;
 
+		// direction
+		this.dx -= Math.pow(opd.earth.radius, 2) / dist * 5 * vx;
+		this.dy += Math.pow(opd.earth.radius, 2) / dist * 5 * vy;
+		if (cnv.key(32)) {
+			this.dx += 200 * vx * ft;
+			this.dy -= 200 * vy * ft;
+		}
+		if (cnv.key(37)) {
+			this.dx += 100 * vx90 * ft;
+			this.dy += 100 * vy90 * ft;
+		} else if (cnv.key(39)) {
+			this.dx -= 100 * vx90 * ft;
+			this.dy -= 100 * vy90 * ft;
+		}
+		// position
 		this.x += this.dx * ft;
 		this.y += this.dy * ft;
-
-		if      (cnv.key(37)) this.x -= 100 * ft;
-		else if (cnv.key(39)) this.x += 100 * ft;
-		if      (cnv.key(38)) this.y -= 100 * ft;
-		else if (cnv.key(40)) this.y += 100 * ft;
 
 		// render
 		this.tail(ctx, cnv, ft);
@@ -74,13 +87,15 @@ opd.ship = {
 					ctx.rotate(angle);
 						this.spShip.draw(0, 0);
 				ctx.restore();
+				// debug
 				ctx.beginPath();
-					ctx.lineWidth = 3;
-					ctx.strokeStyle = "#fff";
+					ctx.lineWidth = 2;
+					ctx.strokeStyle = "rgba(255,255,255,.3)";
 					ctx.globalAlpha = 1;
-					ctx.moveTo(0, 0);
-					ctx.lineTo(this.dx, this.dy);
-					ctx.stroke();
+					ctx.moveTo(0, 0); ctx.lineTo(this.dx, this.dy);
+					ctx.moveTo(0, 0); ctx.lineTo(vx90 * +50, vy90 * +50);
+					ctx.moveTo(0, 0); ctx.lineTo(vx90 * -50, vy90 * -50);
+					// ctx.stroke();
 				ctx.closePath();
 		ctx.restore();
 	}
